208216b02bbf68b9f590e0d8cbc5c80410d047e1
OceanOaden <64564792+OceanOaden@users.noreply.github.com>
2023-09-08 02:01:47 +1000

    Login and Signup page rough design made

--- communityApp/App.js
+++ communityApp/App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Homepage from './Screens/Homepage';
 import Header from './Screens/Header';
+import LoginScreen from './Screens/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      <Homepage />
+      <LoginScreen />
     </View>
   );
 }
