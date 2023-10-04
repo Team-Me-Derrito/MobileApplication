04566f5a09bddc36eb4349c61bef6efccfed3c45
OceanOaden <64564792+OceanOaden@users.noreply.github.com>
2023-10-04 17:08:22 +1000

    Get request deleted

--- communityApp/API/BaseRequest.js
+++ communityApp/API/BaseRequest.js
@@ -1,30 +1,5 @@
 import { BASE_URL, API_KEY } from '../constants/Database.js';
 
-export async function getRequest(endpoint, message) {
-    if (typeof(endpoint) != 'string') {
-        throw new TypeError('Invalid endpoint of type ' + typeof(endpoint));
-    }
-    if (typeof(message) != 'object') {
-        throw new TypeError('Invalid message of type ' + typeof(message));
-    }
-
-    try {
-        const url = `${BASE_URL}/${endpoint}`;
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(message)
-        });
-        const json = await response.json();
-        return json;
-    } catch (err) {
-        console.log(err);
-        return null;
-    }
-}
 
 export async function postRequest(endpoint, message) {
     if (typeof(endpoint) != 'string') {
