ed277965c93717d95600d0833c813be4032a0fc8
OceanOaden <64564792+OceanOaden@users.noreply.github.com>
2023-10-04 15:14:33 +1000

    constants folder added

---
+++ communityApp/constants/Database.js
@@ -0,0 +1,13 @@
+//API
+export const BASE_URL = 'example/api';
+export const API_KEY = 'api_key';
+
+//Account entity
+export const COMMUNITY_ID = 'CommunityID';
+export const NAME = 'Name';
+export const AGE = 'Age';
+export const GENDER = 'Gender';
+export const PHONE_NUMBER = 'Phone Number';
+export const EMAIL = 'Email';
+export const PASSWORD = 'Password';
+export const SALT = 'Salt';
